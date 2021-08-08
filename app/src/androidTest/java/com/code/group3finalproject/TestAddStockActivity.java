f52616ac94665e41e3214021d351a9d36b224464
khim1306 <khim1306@mylaurier.ca>
2021-08-08 10:14:16 -0400

    UML Initial commit

--- app/src/androidTest/java/com/code/group3finalproject/TestAddStockActivity.java
+++ app/src/androidTest/java/com/code/group3finalproject/TestAddStockActivity.java
@@ -15,7 +15,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4.class)
 class TestAddStockActivity {
 
     @Rule
